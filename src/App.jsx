507f5a819cee899f037aaf25fad960d3e7731946
Flavio Oria <flavio.oriap@gmail.com>
2022-12-11 21:45:08 +0100

    Add hook effect to get data countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from 'react'
 
 export default function App() {
-    const urlByName = 'https://restcountries.com/v3.1/name/';
-    const urlByCode = 'https://restcountries.com/v3.1/alpha?codes=col,pe,arg';
+  const urlByName = 'https://restcountries.com/v3.1/name/';
+  const urlByCode = 'https://restcountries.com/v3.1/alpha?codes=col,pe,arg';
 
-    const [queryCountry, setQueryCountry] = useState('');
-    const [countries, setCountries] = useState([]);
+  const [queryCountry, setQueryCountry] = useState('');
+  const [countries, setCountries] = useState([]);
 
-    
-    const handleQueryCountrie = (e) => {
-        setQueryCountrie(e.target.value);
+
+  useEffect(() => {
+    if (queryCountry.length >= 3) {
+      return async () => {
+        console.log('entra aquí')
+        const response = await fetch(`${urlByName}/${queryCountry}`);
+        const data = await response.json();
+
+        console.log(data);
+      }
     }
+  },[queryCountry])
+
+
+  const handleQueryCountrie = (e) => {
+    setQueryCountry(e.target.value);
+    console.log(queryCountry);
+  }
 
   return (
     <>
-        <h1>Descubriendo el mundo</h1>
-        <section>
-            <input type="search" name="searchCountry" placeholder='España' />
-            <button>Buscar</button>
-        </section>
-        <section>
+      <h1>Descubriendo el mundo</h1>
+      <section>
+        <input type="search" name="searchCountry" placeholder='España' onChange={handleQueryCountrie}/>
+        <button>Buscar</button>
+      </section>
+      <section>
 
-        </section>
+      </section>
     </>
   )
 }
