5c765bb4a5d017f88e1b349b6381178df09f5919
Flavio Oria <flavio.oriap@gmail.com>
2022-12-17 02:26:06 +0100

    Add new layout for navbar with search and dropdown

--- src/layouts/navbar-layout/NavBarFields.jsx
+++ src/layouts/navbar-layout/NavBarFields.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { SearchBar } from '../../components/search_bar/SearchBar';
-import { SelectRegion } from '../../components/dropdown_bar/SelectRegion';
+import React from "react";
+import { SearchBar } from "../../components/search_bar/SearchBar";
+import { SelectRegion } from "../../components/dropdown_bar/SelectRegion";
 
 export default function NavBarFields({
   query,
